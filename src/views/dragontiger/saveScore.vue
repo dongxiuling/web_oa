65d1aee28552f7d00ba9d2cdcc4da50a4191b4bf
xiecheng328 <xiecheng328@163.com>
2021-10-29 13:56:57 +0800

    龙虎榜

--- src/views/dragontiger/saveScore.vue
+++ src/views/dragontiger/saveScore.vue
@@ -110,7 +110,7 @@
         </el-table-column>
       </el-table>
 
-      <el-form-item>
+      <el-form-item class="toolbar">
         <el-button type="primary" @click="updateHandle('activity')"
           >保存成绩</el-button
         >
@@ -297,5 +297,9 @@ export default {
 <style scoped>
 .row {
   align-items: center;
+}
+.toolbar {
+  padding: 10px 10px 0 0;
+  text-align: right;
 }
 </style>
